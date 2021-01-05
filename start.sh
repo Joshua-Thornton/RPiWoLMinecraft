a027cc600d26f22a5de582968f565fb9e62c281a
Joshua Thornton <joshdthornton19@gmail.com>
2021-01-05 21:54:10 +1100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 port=ENTER PORT HERE
 socat -u tcp-l:$port,fork system:./command.sh &
 echo "Attempted to start socat in the background"
-echo "You can check with ps -aux"
+echo "You can check with ps -C socat"
+echo "If a number appears under PID, it's working"
